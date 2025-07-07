5df7e5b0bd4a344fb522436daab4557574e765bb
Maximus7474 <the.maximus.7474@gmail.com>
2025-07-07 18:02:29 +0200

    feat(database): define transactions, transaction_packages, and customer_developers tables with foreign key constraints

--- src/utils/database/sqlite/base.sql
+++ src/utils/database/sqlite/base.sql
@@ -1,7 +1,35 @@
--- Placeholder table as example
-CREATE TABLE IF NOT EXISTS users (
+PRAGMA foreign_keys=ON;
+
+BEGIN TRANSACTION;
+
+CREATE TABLE IF NOT EXISTS `transactions` (
     `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-    `name` TEXT NOT NULL,
+    `tbxid` TEXT UNIQUE NOT NULL,
     `email` TEXT UNIQUE NOT NULL,
+    `chargeback` INTEGER DEFAULT 0,
+    `discord_id` TEXT NOT NULL,
+    `webstore` TEXT NOT NULL,
+    `purchaser_name` TEXT NOT NULL,
+    `purchaser_uuid` TEXT NOT NULL,
     `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS `transaction_packages` (
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `tbxid` TEXT NOT NULL,
+    `package` TEXT NOT NULL,
+
+    FOREIGN KEY (`tbxid`) REFERENCES transactions(`tbxid`)
+);
+
+CREATE TABLE IF NOT EXISTS `customer_developers` (
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `tbxid` TEXT NOT NULL,
+    `discord_id` TEXT NOT NULL,
+    `added_by` TEXT NOT NULL,
+    `added_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (`tbxid`) REFERENCES transactions(`tbxid`)
+);
+
+COMMIT;
