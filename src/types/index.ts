3e2d0c627c2aae6a0332ef274327cff0eb9801ff
Maximus7474 <the.maximus.7474@gmail.com>
2025-07-07 17:19:17 +0200

    chore(types): rename tebex-api to tebex_api to normalize naming convention

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,4 +2,4 @@ export * from './client';
 export * from './event_handler';
 export * from './command_handler';
 export * from './static_messages';
-export * from './tebex-api';
+export * from './tebex_api';
