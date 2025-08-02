3652d80d43ba2748ee333a4f4a652e87e00168ca
Maximus7474 <the.maximus.7474@gmail.com>
2025-08-02 18:57:32 +0200

    chore: replace default terminal colour functions with colors package

---
+++ src/utils/colours.ts
@@ -0,0 +1,12 @@
+import Colors from 'colors';
+
+export const black = Colors.black;
+export const red = Colors.red;
+export const green = Colors.green;
+export const yellow = Colors.yellow;
+export const blue = Colors.blue;
+export const magenta = Colors.magenta;
+export const cyan = Colors.cyan;
+export const white = Colors.white;
+export const gray = Colors.gray;
+export const grey = Colors.grey;
