104f9b8ee8c4c96ef76e17a7648dba91e0019d4d
Maximus7474 <the.maximus.7474@gmail.com>
2025-08-02 19:41:51 +0200

    chore(index): removed database initialization

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,10 @@ import { Client, Collection, GatewayIntentBits, Partials } from 'discord.js';
 import { DiscordClient } from './types';
 
 import Config from './utils/config';
-import Database from './utils/database';
 
 import LoadCommands from './utils/initialisation/load_commands';
 import LoadEvents from './utils/initialisation/load_events';
 import LoadStaticMessages from './utils/initialisation/load_static_messages';
-
-if (Database) Database.init();
 
 const client: DiscordClient = new Client({
     intents: [
