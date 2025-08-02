64facc4f6e6006943553b57a7dd9b1e6c429a30d
Maximus7474 <the.maximus.7474@gmail.com>
2025-08-02 19:39:12 +0200

    fix(events): added typing for default export

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,3 +1,4 @@
+import EventHandler from "../classes/event_handler";
 import command_handler from "./command_handler";
 import logging from "./logging";
 import ready from "./ready";
@@ -6,4 +7,4 @@ export default [
   logging,
   command_handler,
   ready,
-];
+] as EventHandler[];
