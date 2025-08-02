2c881baa68ae083d9a0fd626955f2c3ef8811d47
Maximus7474 <the.maximus.7474@gmail.com>
2025-08-02 19:01:13 +0200

    feat(types): database connector interface

---
+++ src/types/database.ts
@@ -0,0 +1,8 @@
+export interface DBConnectionDetails {
+    SQLITE_PATH?: string;
+    SQL_HOST?: string;
+    SQL_PORT?: number;
+    SQL_USER?: string;
+    SQL_DATABASE?: string;
+    SQL_PASSWORD?: string;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,4 @@ export * from './command_handler';
 export * from './static_messages';
 export * from './tebex_api';
 export * from './transaction_logging';
+export * from './database';
