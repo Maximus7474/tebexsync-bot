20bcb32bd47981c241387ba25f4489da55f2dc94
Maximus7474 <the.maximus.7474@gmail.com>
2025-08-02 19:06:26 +0200

    refactor(static_messages): initialisation

--- src/utils/initialisation/load_static_messages.ts
+++ src/utils/initialisation/load_static_messages.ts
@@ -1,64 +1,47 @@
-import * as path from 'path';
 import { DiscordClient } from '../../types';
-import { getFilesFromDir } from '../utils';
+import { AnySelectMenuInteraction, ButtonInteraction } from 'discord.js';
+import static_messages from '../../static_messages';
 
 import Logger from '../logger';
-import StaticMessage from '../../classes/static_messages';
-import { AnySelectMenuInteraction, ButtonInteraction } from 'discord.js';
-import { pathToFileURL } from 'url';
 const logger = new Logger('LoadStaticMessages');
 
 export default (client: DiscordClient) => {
-  const initialiseStaticMessage = async (client: DiscordClient) => {
-    const callbackHandler = new Map<string, (client: DiscordClient, interaction: ButtonInteraction | AnySelectMenuInteraction) => Promise<void>>();
+    const initialiseStaticMessage = async (client: DiscordClient) => {
+        const callbackHandler = new Map<string, (client: DiscordClient, interaction: ButtonInteraction|AnySelectMenuInteraction) => Promise<void>>();
+    
+        static_messages.forEach(async (message) => {
+            try {
+                message.initialize(client)
+                .catch((err) => {
+                    logger.error('Unable to initialize', message.name, 'error:', err.message);
+                });
 
-    const staticMessageDir = path.join(__dirname, '../../static_messages');
-    const messages = getFilesFromDir(staticMessageDir);
+                message.customIds.forEach((customId) => {
+                    callbackHandler.set(customId, message.handleInteraction.bind(message));
+                });
 
-    messages.forEach(async (file) => {
-      const filePath = path.join(file);
-      const fileUrl = pathToFileURL(filePath).href;
+                logger.success(`Loaded static message: ${message.name}`)
+            } catch (error) {
+                console.error(`Failed to load static message: ${message.name}\n`, error);
+            }
+        });
+    
+        client.on('interactionCreate', async (interaction) => {
+            if (interaction.isButton() || interaction.isAnySelectMenu()) {
+                const { customId } = interaction;
+                const handler = callbackHandler.get(customId);
+                console.log(customId, callbackHandler.keys());
+                if (handler) {
+                    await handler(client, interaction);
+                } else {
+                    logger.warn(`No handler found for static message: ${customId}`);
+                }
+            }
+        });
+    }
 
-      try {
-        const staticMessageModule = await import(fileUrl);
-
-        if (staticMessageModule && staticMessageModule.default) {
-          const { default: message } = staticMessageModule as { default: StaticMessage };
-
-          message.initialize(client)
-            .catch((err) => {
-              logger.error('Unable to initialize', path.basename(filePath), 'error:', err.message);
-            });
-
-          message.customIds.forEach((customId) => {
-            callbackHandler.set(customId, message.handleInteraction.bind(message));
-          });
-
-          logger.success(`Loaded static message: ${message.name}`)
-        } else {
-          logger.warn(`Unable to static message: ${filePath.slice(staticMessageDir.length + 1)}`)
-        }
-      } catch (error) {
-        console.error(`Failed to load static message: ${file}\n`, error);
-      }
+    client.once('ready', async () => {
+        await initialiseStaticMessage(client);
     });
-
-    client.on('interactionCreate', async (interaction) => {
-      if (interaction.isButton() || interaction.isAnySelectMenu()) {
-        const { customId } = interaction;
-        const handler = callbackHandler.get(customId);
-        console.log(customId, callbackHandler.keys());
-        if (handler) {
-          await handler(client, interaction);
-        } else {
-          logger.warn(`No handler found for static message: ${customId}`);
-        }
-      }
-    });
-  }
-
-  client.once('ready', async () => {
-    await initialiseStaticMessage(client);
-  });
 }
 
