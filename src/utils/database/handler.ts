cd6e23ff6f4054944aac305a4043cfd63c57b01a
Maximus7474 <the.maximus.7474@gmail.com>
2025-08-02 19:32:52 +0200

    chore: reimplement sqlite database handler

--- src/utils/database/handler.ts
+++ src/utils/database/handler.ts
@@ -1,37 +1,174 @@
+import Database from 'better-sqlite3';
+import { readFileSync, existsSync } from 'fs';
+import path from 'path';
 import { DBConnectionDetails } from '../../types';
-import { red } from 'colors';
 
+import Logger from '../logger';
+const logger = new Logger('SQLiteHandler');
+
+/*
+    Better SQLITE 3 is a syncronous module, to follow the same structure for
+    the database handler class we're using Promises to "fake" the asyncronous
+    behaviour to allow for better switching capability of handlers.
+*/
 export default class SQLiteHandler {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  constructor(data: DBConnectionDetails) {
-    console.error(red('[ERROR] No database handler installed !'));
-    console.error(red('[ERROR] Use the setup-db script to install one.'));
+  private db: Database.Database;
+
+  constructor({ SQLITE_PATH }: DBConnectionDetails) {
+    if (typeof SQLITE_PATH !== 'string') {
+      throw new Error(`DB Path for SQLite database is invalid !`);
+    }
+
+    this.db = new Database(SQLITE_PATH);
   }
 
-  initializeDB(): void { }
+  /**
+   * Sets up the database by running the base.sql file to
+   * run all queries listed in it
+   * @returns {Promise<void>}
+   */
+  async initializeDB(): Promise<void> {
+    const scriptPath = path.join(import.meta.dirname, 'base-sqlite.sql');
+    let sqlScript: string;
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  run(query: string, params: unknown[] = []): void {
-    console.error(red('[ERROR] No database handler installed !'));
-    console.error(red(`[ERROR] Unable to run query: ${query}.`));
+    if (existsSync(scriptPath)) {
+      sqlScript = readFileSync(scriptPath, 'utf8');
+    } else {
+      logger.warn(`SQLite base script not found at ${scriptPath}. Skipping database initialization.`);
+      return Promise.reject(new Error(`SQLite base script not found at ${scriptPath}`));
+    }
+
+    try {
+      this.db.exec(sqlScript);
+      return Promise.resolve();
+    } catch (error) {
+      logger.error(`Error initializing database: ${error}`);
+      return Promise.reject(error);
+    }
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  get(query: string, params: unknown[] = []): unknown {
-    console.error(red('[ERROR] No database handler installed !'));
-    console.error(red(`[ERROR] Unable to run get query: ${query}.`));
-    return {};
+  /**
+   * Executes a SQL query that does not return any result (e.g., INSERT, UPDATE, DELETE).
+   *
+   * @param query - The SQL query string to be executed.
+   * @param params - An optional array of parameters to bind to the query placeholders.
+   *                 Defaults to an empty array if not provided.
+   *
+   * @throws {Error} If the query execution fails.
+   * @returns {Promise<void>}
+   */
+  async execute(query: string, params: unknown[] = []): Promise<void> {
+    try {
+      const stmt = this.db.prepare(query);
+      stmt.run(...params);
+      return Promise.resolve();
+    } catch (error) {
+      logger.error(`Error running query "${query}": ${error}`);
+      return Promise.reject(error);
+    }
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  all(query: string, params: unknown[] = []): unknown[] {
-    console.error(red('[ERROR] No database handler installed !'));
-    console.error(red(`[ERROR] Unable to run all query: ${query}.`));
-    return [];
+  /**
+   * Performs an INSERT operation and returns the ID of the newly inserted row.
+   * This relies on SQLite's `last_insert_rowid()` or `AUTOINCREMENT` behavior.
+   *
+   * @param {string} query - The SQL INSERT query string.
+   * @param {unknown[]} params - An array of parameters to bind to the query.
+   *
+   * @returns {Promise<number | null>} A Promise that resolves with the ID of the last inserted row,
+   * or the number of affected rows.
+   * @throws {Error} If the insert query execution fails.
+   */
+  async insert(query: string, params: unknown[]): Promise<number | null> {
+    try {
+      const stmt = this.db.prepare(query);
+      const result = stmt.run(...params);
+
+      if (result.lastInsertRowid !== undefined && result.lastInsertRowid !== null) {
+        return Promise.resolve(Number(result.lastInsertRowid));
+      } else {
+        logger.warn(`Insert query "${query}" did not return a last inserted ID.`);
+        return Promise.resolve(result.changes);
+      }
+    } catch (err) {
+      logger.error(`Error inserting data with query "${query}":`, err);
+      return Promise.reject(err);
+    }
   }
 
-  close(): void {
-    console.error(red('[ERROR] No database handler installed !'));
-    console.error(red('[ERROR] Unable to close connection.'));
+  /**
+   * Executes an UPDATE SQL query and returns the number of rows affected.
+   *
+   * @param {string} query - The SQL UPDATE query string.
+   * @param {unknown[]} params - An array of parameters to bind to the query.
+   * @returns {Promise<number>} A Promise that resolves with the number of rows affected by the update.
+   * @throws {Error} If the update query execution fails.
+   */
+  async update(query: string, params: unknown[]): Promise<number> {
+    try {
+      const stmt = this.db.prepare(query);
+      const result = stmt.run(...params);
+
+      return Promise.resolve(result.changes);
+    } catch (err) {
+      logger.error(`Error updating data with query "${query}":`, err);
+      return Promise.reject(err);
+    }
+  }
+
+  /**
+   * Executes a SQL query to retrieve a single row from the database.
+   *
+   * @template T The expected type of the returned data by the query.
+   * @param query - The SQL query string to be executed.
+   * @param params - An optional array of parameters to bind to the query.
+   *
+   * @throws {Error} If the query execution fails.
+   * @returns The first row of the result set as an object, or `undefined` if no rows are found.
+   */
+  async get<T>(query: string, params: unknown[] = []): Promise<T | undefined> {
+    try {
+      const stmt = this.db.prepare(query);
+      return Promise.resolve(stmt.get(...params) as T | undefined);
+    } catch (error) {
+      logger.error(`Error getting data with query "${query}": ${error}`);
+      return Promise.reject(error);
+    }
+  }
+
+  /**
+   * Executes a SQL query and retrieves all matching rows from the database.
+   *
+   * @template T The expected type of the returned data by the query.
+   * @param query - The SQL query string to execute.
+   * @param params - An optional array of parameters to bind to the query. Defaults to an empty array.
+   *
+   * @throws {Error} If the query execution fails.
+   * @returns An array of objects representing the rows returned by the query.
+   */
+  async all<T>(query: string, params: unknown[] = []): Promise<T[]> {
+    try {
+      const stmt = this.db.prepare(query);
+      return Promise.resolve(stmt.all(...params) as T[]);
+    } catch (error) {
+      logger.error(`Error getting all data with query "${query}": ${error}`);
+      return Promise.reject(error);
+    }
+  }
+
+  /**
+   * Closes the connection to the database
+   *
+   * @throws {Error} If the query execution fails.
+   * @returns
+   */
+  async close(): Promise<void> {
+    try {
+      this.db.close();
+      return Promise.resolve();
+    } catch (error) {
+      logger.error(`Error closing database: ${error}`);
+      return Promise.reject(error);
+    }
   }
 }
