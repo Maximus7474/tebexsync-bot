38ffe058e5d335748de6f155a83cc6851f6541c5
Maximus7474 <the.maximus.7474@gmail.com>
2025-08-15 15:08:06 +0200

    feat(customer_commands): add developer command

---
+++ src/commands/customer_commands/add_developer.ts
@@ -0,0 +1,85 @@
+import { MessageFlags, SlashCommandBuilder } from "discord.js";
+import SlashCommand from "../../classes/slash_command";
+import Database from "../../utils/database";
+import settings_handler from "../../handlers/settings_handler";
+
+export default new SlashCommand({
+  name: 'add-developer',
+  guildSpecific: true,
+  slashcommand: new SlashCommandBuilder()
+    .setName('adddeveloper')
+    .setDescription('Add a developer to also gain access to support channels.')
+    .addUserOption(o =>
+      o.setName('member')
+      .setDescription('Member to add the developer role')
+      .setRequired(true)
+    ),
+  callback: async (logger, client, interaction) => {
+    const { user, options, guild } = interaction;
+
+    if (!guild) {
+      interaction.reply({
+        content: "This command can only be used on a server.",
+        flags: MessageFlags.Ephemeral
+      });
+      return;
+    }
+
+    const purchase = await Database.get<{ tbxid: string }>(
+      'SELECT `tbxid` FROM `transactions` WHERE `discord_id` = ? AND `refund` = 0 AND `chargeback` = 0',
+      [ user.id ]
+    );
+
+    if (!purchase) {
+      interaction.reply({
+        content: 'No linked purchases',
+        flags: MessageFlags.Ephemeral,
+      });
+      return;
+    }
+
+    const currentDevs = await Database.get<{count: number}>(
+      'SELECT COUNT(`discord_id`) AS `count` FROM `customer_developers` WHERE `tbxid` = ?',
+      [purchase.tbxid]
+    );
+
+    if (currentDevs?.count && currentDevs.count > (settings_handler.get('max_developers') as number)) {
+      interaction.reply({
+        content: `You've already added the maximum amount of developers for your purchase.`,
+        flags: MessageFlags.Ephemeral,
+      });
+      return;
+    }
+
+    const developerId = options.getUser('member', true);
+    const developer = await guild.members.fetch({ user: developerId, cache: false });
+
+    if (!developer) {
+      interaction.reply({
+        content: `<@${developerId.id}> was not found on the server.`,
+        flags: MessageFlags.Ephemeral,
+      });
+      return;
+    }
+
+    const devRoleId = settings_handler.get('customers_dev_role') as string;
+    const role = await guild.roles.fetch(devRoleId);
+    if (role) {
+      await developer.roles.add(role, 'Added as developer for purchase');
+    } else {
+      logger.error(`Unable to grant customer's developer role, role with ID ${devRoleId} was not found.`);
+      interaction.reply({
+        content: `Unable to grant customer's developer role, please notify server staff that the bot isn't setup properly.`,
+        flags: MessageFlags.Ephemeral,
+      });
+      return;
+    }
+
+    interaction.reply({
+      content: `Customer's developer role was granted to: <@${developer.id}>`,
+      flags: MessageFlags.Ephemeral,
+    });
+
+    logger.success(`${developer.user.username} (${developer.id}) was added to purchase ${purchase.tbxid} as developer by ${user.username} (${user.id})`);
+  }
+});
