57987af01cc6c4146026a7f1277b621601e1a67f
Maximus7474 <the.maximus.7474@gmail.com>
2025-08-15 10:41:49 +0200

    tweak(events/ready): fetch app data on startup

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,7 +5,8 @@ export default new EventHandler({
   name: 'READY',
   eventName: Events.ClientReady,
   type: "once",
-  callback: (logger, client) => {
+  callback: async (logger, client) => {
+    await client.application?.fetch();
     logger.success(`Booted up and logged in as @${client.user?.username}#${client.user?.discriminator}`)
   }
 });
