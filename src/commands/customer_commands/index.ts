7fc27ab6f0644332f231b88a1f6423534773bb75
Maximus7474 <the.maximus.7474@gmail.com>
2025-08-15 16:02:42 +0200

    feat(customer_commands): remove developer command

--- src/commands/customer_commands/index.ts
+++ src/commands/customer_commands/index.ts
@@ -1,10 +1,12 @@
 import SlashCommand from "../../classes/slash_command";
 import add_developer from "./add_developer";
 import claim_role from "./claim_role";
+import remove_developer from "./remove_developer";
 import view_developers from "./view_developers";
 
 export default [
   claim_role,
   view_developers,
   add_developer,
+  remove_developer,
 ] as SlashCommand[];
---
+++ src/commands/customer_commands/remove_developer.ts
@@ -0,0 +1,85 @@
+import { MessageFlags, SlashCommandBuilder } from "discord.js";
+import SlashCommand from "../../classes/slash_command";
+import Database from "../../utils/database";
+import settings_handler from "../../handlers/settings_handler";
+
+export default new SlashCommand({
+  name: 'remove-developer',
+  guildSpecific: true,
+  slashcommand: new SlashCommandBuilder()
+    .setName('removedeveloper')
+    .setDescription('Remove a developer that is linked to your purchase.')
+    .addUserOption(o =>
+      o.setName('member')
+      .setDescription('Member to remove the developer role from')
+      .setRequired(true)
+    ),
+  callback: async (logger, client, interaction) => {
+    const { user, guild, options } = interaction;
+
+    if (!guild) {
+      interaction.reply({
+        content: "This command can only be used on a server.",
+        flags: MessageFlags.Ephemeral
+      });
+      return;
+    }
+
+    const purchase = await Database.get<{ tbxid: string }>(
+      'SELECT `tbxid` FROM `transactions` WHERE `discord_id` = ? AND `refund` = 0 AND `chargeback` = 0',
+      [ user.id ]
+    );
+
+    if (!purchase) {
+      interaction.reply({
+        content: 'No linked purchases',
+        flags: MessageFlags.Ephemeral,
+      });
+      return;
+    }
+
+    const currentDevs = await Database.all<{discord_id: string}>(
+      'SELECT `discord_id` AS `count` FROM `customer_developers` WHERE `tbxid` = ?',
+      [purchase.tbxid]
+    );
+
+    if (currentDevs.length === 0) {
+      interaction.reply({
+        content: `You have no linked developers.`,
+        flags: MessageFlags.Ephemeral,
+      });
+      return;
+    }
+
+    const developer = options.getUser('member', true);
+
+    const listed = currentDevs.find(({ discord_id }) => discord_id === developer.id);
+
+    if (!listed) {
+      interaction.reply({
+        content: `<@${developer.id}> is not listed as your developer.`,
+        flags: MessageFlags.Ephemeral,
+      });
+      return;
+    }
+
+    Database.execute(
+      'DELETE FROM `customer_developers` WHERE `tbxid` = ? AND `discord_id` = ?',
+      [purchase.tbxid, developer.id]
+    );
+
+    const member = await guild.members.fetch({ user: developer, cache: false });
+
+    if (member) {
+      const customersDevRole = settings_handler.get('customers_dev_role') as string;
+      await member.roles.remove(customersDevRole)
+    }
+
+    interaction.reply({
+      content: `<@${developer.id}> has been removed from your developers.`,
+      flags: MessageFlags.Ephemeral,
+    });
+
+    logger.success(`${developer.username} (${developer.id}) was removed from purchase ${purchase.tbxid} as developer by ${user.username} (${user.id})`);
+  }
+});
