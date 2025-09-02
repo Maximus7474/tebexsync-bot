82a2d1fc54adb334d6724e77edcd605b76b3edbe
Maximus7474 <the.maximus.7474@gmail.com>
2025-09-03 00:54:40 +0200

    tweak(database/base-sqlite): alter file structure

--- src/utils/database/base-sqlite.sql
+++ src/utils/database/base-sqlite.sql
@@ -1,8 +1,24 @@
+/*
+    GLOBAL TABLES & DATA
+ */
+
 CREATE TABLE IF NOT EXISTS `settings` (
     `name` TEXT PRIMARY KEY,
     `data_type` TEXT NOT NULL,
     `value` TEXT NOT NULL
 );
+
+/* Default settings */
+INSERT OR IGNORE INTO `settings` (`name`, `data_type`, `value`) VALUES
+    ('customer_role', 'string', '1354857644174872737'),
+    ('customers_dev_role', 'string', '1354857695316017334'),
+    ('payment_log_channel', 'string', '1209137239217537126'),
+    ('notifying_discord_id', 'string', '336592756698906626'),
+    ('max_developers', 'number', 2);
+
+/*
+    CUSTOMERS & TRANSACTIONS
+ */
 
 CREATE TABLE IF NOT EXISTS `customers` (
     `id` INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -41,11 +57,3 @@ CREATE TABLE IF NOT EXISTS `customer_developers` (
     UNIQUE(customer_id, discord_id),
     FOREIGN KEY (`customer_id`) REFERENCES customers(`id`)
 );
-
-/* Default settings */
-INSERT OR IGNORE INTO `settings` (`name`, `data_type`, `value`) VALUES
-    ('customer_role', 'string', '1354857644174872737'),
-    ('customers_dev_role', 'string', '1354857695316017334'),
-    ('payment_log_channel', 'string', '1209137239217537126'),
-    ('notifying_discord_id', 'string', '336592756698906626'),
-    ('max_developers', 'number', 2);
