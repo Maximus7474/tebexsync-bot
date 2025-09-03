33e9ea7241f88fd0ec1705ac2c0c2d93ac9bf1e1
Maximus7474 <the.maximus.7474@gmail.com>
2025-09-03 17:57:18 +0200

    feat(types): ticket types

---
+++ src/types/tickets.ts
@@ -0,0 +1,31 @@
+export interface TicketCategory {
+  id: number;
+  name: string;
+  description: string;
+  emoji: string | null;
+  category_channel: string;
+  require_tbxid: 1 | 0;
+}
+
+export interface TicketCategoryField {
+  id: number;
+  label: string;
+  placeholder: string;
+  required: 1 | 0;
+  short_field: 1 | 0;
+  min_length: 1 | 0 | null;
+  max_length: 1 | 0 | null;
+}
+
+export interface TicketCategoryData extends TicketCategory {
+  fields: TicketCategoryField[];
+}
+
+export interface DatabaseTicket {
+  category: number;
+  ticket_name: string;
+
+  user_id: string;
+  user_username: string;
+  user_display_name: string;
+}
