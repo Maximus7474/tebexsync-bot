53aa39d78ced5f98015c1d64e182949a49861d16
Maximus7474 <the.maximus.7474@gmail.com>
2025-09-05 15:15:36 +0200

    refactor(settings_handler): track data type for a settings

--- src/handlers/settings_handler.ts
+++ src/handlers/settings_handler.ts
@@ -3,25 +3,28 @@ import Logger from "../utils/logger";
 
 const logger = new Logger('Settings Manager');
 
+export type SettingDataDisplayTypes = 'number' | 'string' | 'object' | 'channel_id' | 'role_id';
+export type SettingDataType = number | string | object | null;
+
 class SettingsManager {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  private settings: Map<string, any> = new Map();
+  private settings: Map<string, { type: SettingDataDisplayTypes; value: any }> = new Map();
 
   constructor() {
     DB.all<{
       name: string;
-      data_type: 'number' | 'string' | 'object',
+      data_type: SettingDataDisplayTypes,
       value: string;
     }>('SELECT * FROM `settings`')
       .then((rawSettings) => {
         for (const { name, data_type, value } of rawSettings) {
-          let parsedData
+          let parsedData: SettingDataType = null;
 
           if (data_type === 'number') {
             parsedData = parseInt(value);
           } else if (data_type === 'object') {
             parsedData = JSON.parse(value);
-          } else if (data_type === 'string') {
+          } else if (data_type === 'string' || data_type === 'role_id' || data_type === 'channel_id') {
             parsedData = value;
           } else {
             logger.error('Invalid "data_type" found in settings table !');
@@ -29,11 +32,14 @@ class SettingsManager {
           }
 
           if (parsedData) {
-            this.settings.set(name, parsedData);
+            this.settings.set(name, {
+              type: data_type,
+              value: parsedData,
+            });
           }
         }
 
-        logger.info(`Loaded ${this.settings.size}/${rawSettings.length} settings from database.`);
+        logger.success(`Loaded ${this.settings.size}/${rawSettings.length} settings from database.`);
       })
       .catch(err => {
         logger.error('Unable to load settings from database:', err.message)
@@ -44,21 +50,32 @@ class SettingsManager {
     return Array.from(this.settings.keys());
   }
 
-  get(key: string): number | string | object | null {
-    return this.settings.get(key) ?? null;
+  get<T>(key: string): T | null {
+    return this.settings.get(key)?.value as T ?? null;
+  }
+
+  getDataType(key: string): SettingDataDisplayTypes | null {
+    return this.settings.get(key)?.type ?? null;
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  set(key: string, value: any): boolean {
-    if (!this.settings.has(key)) return false;
+  set(key: string, value: any) {
+    if (!this.settings.has(key)) {
+      logger.error(`Tried to set an inexistant setting: ${key}`);
+      return;
+    }
 
     DB.execute(
       'UPDATE `settings` SET `value` = ? WHERE `name` = ?',
       [value, key]
     );
 
-    this.settings.set(key, value);
-    return true;
+    const data_type = this.settings.get(key)!.type;
+
+    this.settings.set(key, {
+      type: data_type,
+      value: value,
+    });
   }
 }
 
