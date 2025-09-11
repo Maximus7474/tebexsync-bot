ee206f3ea694f71b37b66f0018476ed442a0b3bb
Maximus7474 <the.maximus.7474@gmail.com>
2025-09-11 21:49:06 +0200

    chore(index): set client instance within PurchaseManager

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,20 @@ import Config from './utils/config';
 import LoadCommands from './utils/initialisation/load_commands';
 import LoadEvents from './utils/initialisation/load_events';
 import LoadStaticMessages from './utils/initialisation/load_static_messages';
+import PurchaseManager from './handlers/purchase_handler';
 
 const client: DiscordClient = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
-    ],
-    partials: [
-        Partials.Message,
-        Partials.Channel,
-        Partials.Reaction,
-    ],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+  partials: [
+    Partials.Message,
+    Partials.Channel,
+    Partials.Reaction,
+  ],
 }) as DiscordClient;
 
 client.commands = new Collection();
@@ -28,5 +29,7 @@ client.autocompleteCommands = new Collection();
 LoadCommands(client);
 LoadEvents(client);
 LoadStaticMessages(client);
+
+PurchaseManager.setDiscordClient(client);
 
 client.login(Config.DISCORD_BOT_TOKEN);
