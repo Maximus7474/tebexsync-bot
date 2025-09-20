8c0a6ac48d161219da7a8b192b1bf0c1d540e202
Maximus7474 <the.maximus.7474@gmail.com>
2025-09-20 12:07:04 +0200

    feat(purchase_handler): check if a customer has valid purchases

--- src/handlers/purchase_handler.ts
+++ src/handlers/purchase_handler.ts
@@ -1,6 +1,8 @@
 import { DiscordClient } from "@types";
 import Database from "../utils/database";
 import Logger from "../utils/logger";
+import env from "../utils/config";
+import SettingsManager from "../handlers/settings_handler";
 
 const logger = new Logger('Purchase Manager');
 
@@ -40,6 +42,93 @@ class PurchaseManager {
     if (!id) throw new Error(`Unable to insert ${discordId} into customers database table.`);
 
     return id;
+  }
+
+  /**
+   * Checks if a customer has valid / active purchases, if not tries to remove his customer role
+   * along with any developers linked to his id. If no entries, active or not, deletes his entry.
+   *
+   * @param customerid
+   * @returns {boolean} has valid purchases
+   */
+  public static async checkCustomerPurchases(customerid: number): Promise<boolean> {
+    const customer = await Database.get<{ id: number; discord_id: string; }>('SELECT `id`, `discord_id` FROM `customers` WHERE `id` = ?', [ customerid ]);
+
+    if (!customer) {
+      logger.warn('checkCustomerPurchases received an inexistant customer id:', customerid, '- exiting...');
+      return false;
+    }
+
+    const purchases = await Database.all<{ id: number; refund: 0 | 1, chargeback: 0 | 1 }>(
+      'SELECT `id`, `refund`, `chargeback` FROM `transactions` WHERE `customer` = ?',
+      [ customer.id ],
+    );
+
+    let hasActivePurchases;
+    if (purchases.length === 0) {
+      hasActivePurchases = false;
+
+      await Database.execute('DELETE FROM `customers` WHERE `id` = ?', [ customer.id ]);
+
+    } else {
+      const activePurchases = purchases.filter(({ chargeback, refund }) => (chargeback !== 1 && refund !== 1) );
+
+      hasActivePurchases = activePurchases.length > 0;
+    }
+
+    if (hasActivePurchases) return true;
+
+    const guild = await this.discordClient.guilds.fetch(env.MAIN_GUILD_ID);
+
+    if (!guild) throw new Error('MAIN_GUILD_ID did not produce a valid guild object !');
+
+    const customerUser = await guild.members.fetch(customer.discord_id);
+
+    if (customerUser) {
+      const customerRole = SettingsManager.get('customer_role') as string;
+
+      customerUser.roles.remove(customerRole)
+      .catch(err => {
+        logger.error(
+          'Unable to remove customer role from',
+          customer.discord_id,
+          'err:', err
+        );
+      });
+    }
+
+    const developers = await Database.all<{ id: number; discord_id: string; }>(
+      'SELECT `id`, `discord_id` FROM `customer_developers` WHERE `customer_id` = ?',
+      [ customer.id ]
+    );
+
+    if (developers.length > 0) {
+      const customersDevRole = SettingsManager.get('customers_dev_role') as string;
+
+      for (const { discord_id } of developers) {
+        const developerUser = await guild.members.fetch(discord_id);
+
+        if (developerUser) {
+          await developerUser.roles.remove(customersDevRole)
+          .catch(err => {
+            logger.error(
+              'Unable to remove customers developer role from',
+              discord_id,
+              'err:', err
+            );
+          });
+        }
+      }
+    }
+
+    await Database.execute(
+      'DELETE FROM `customer_developers` WHERE `customer_id` = ?',
+      [ customer.id ]
+    );
+
+    logger.info(`Customer (${customer.id}) no longer has any active purchases`)
+
+    return false;
   }
 }
 
